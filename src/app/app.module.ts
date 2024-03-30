d5eee71e29fcc7e2c773e4eff3b0e8e3b0b607ed
Lucas angelo <angelolucas374@gmail.com>
2024-03-30 08:40:27 -0300

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import  {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
+//PrimeNg
+import {CardModule} from "primeng/card"
+import {InputTextModule} from "primeng/inputtext"
+import {ButtonModule} from "primeng/button"
+import {ToastModule} from "primeng/toast"
+
+//Rotas
 import { AppRoutingModule } from './app-routing.module';
+
+//Component
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    //PrimeNg
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
